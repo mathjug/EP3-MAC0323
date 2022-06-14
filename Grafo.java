8f80eef6cc0543c1641bcb3effcf83191c988407
mathjug <mjurgensen@usp.br>
2022-06-14 17:46:26 -0300

    implementa grafo de inteiros funcional e com gerador aleatório.

--- Grafo.java
+++ Grafo.java
@@ -1,8 +1,9 @@
 import java.util.*;
 import java.io.*;
 import java.util.Scanner;
+import java.util.Random;
 
-public class Grafo<Item> {
+public class Grafo {
     /*
     Classe que implementa um grafo, com os seguintes atributos:
         V = número de vértices
@@ -11,7 +12,41 @@ public class Grafo<Item> {
     */
     private int V;
     private int E;
-    private NodeGrafo<Item>[] arestas;
+    private NodeGrafo[] arestas;
+
+    private class NodeGrafo {
+        /*
+        Classe que implementa um nó utilizado para implementar um grafo, contido no array
+        de arestas.
+        */
+        private int valor; // valor do vértice
+        private NodeGrafo proximo;
+        private int n_adjacentes; // guarda o número de vértices adjacentes (é -1 caso seja um nó na lista dos adjacentes)
+
+        private NodeGrafo(int valor) {
+            this.valor = valor;
+            proximo = null;
+            n_adjacentes = -1;
+        }
+
+        private int nAdjacentes() {
+            return n_adjacentes;
+        }
+        private NodeGrafo proximo() {
+            return proximo;
+        }
+        private int valor() {
+            return valor;
+        }
+
+        private void add(int v) {
+            NodeGrafo aux = this;
+            for (int i = 0; i < this.n_adjacentes; i++)
+                aux = aux.proximo;
+            aux.proximo = new NodeGrafo(v);
+            this.n_adjacentes++;
+        }
+    }
 
     public Grafo(String endereco) throws IOException {
         File arquivo = new File(endereco);
@@ -19,17 +54,51 @@ public class Grafo<Item> {
         
         V = input.nextInt();
         E = input.nextInt();
-        input.nextLine();
-        arestas = (NodeGrafo<Item>[]) new Object[V];
+        //input.nextLine(); // apenas para pular para a próxima linha
+        arestas = new NodeGrafo[V];
+        for (int v = 0; v < V; v++) {
+            arestas[v] = new NodeGrafo(v);
+            arestas[v].n_adjacentes = 0;
+        }
 
         for (int e = 0; e < E; e++) {
-            
+            int u = input.nextInt();
+            int v = input.nextInt();
+            arestas[u].add(v);
+            arestas[v].add(u);
         }
-        
         input.close();
     }
 
-    public Grafo(int V, int E, NodeGrafo<Item>[] arestas) {
+    public Grafo(int V, double p) {
+        /*
+        Construtor que, recebendo o número de vértices e a probabilidade p de cada uma das arestas
+        existir, gera um grafo (0 <= p <= 1).
+        */
+        this.V = V;
+        arestas = new NodeGrafo[V];
+        for (int v = 0; v < V; v++) {
+            arestas[v] = new NodeGrafo(v);
+            arestas[v].n_adjacentes = 0;
+        }
+        Random rand = new Random();
+        int e = 0;
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (i != j) {
+                    double prob = rand.nextDouble();
+                    if (prob < p) {
+                        arestas[i].add(j);
+                        arestas[j].add(i);
+                        e++;
+                    }
+                }
+            }
+        }
+        E = e;
+    }
+
+    public Grafo(int V, int E, NodeGrafo[] arestas) {
         /*
         Construtor que, recebendo o número de vértices, o número de arestas e o array de arestas,
         gera um grafo com esses atributos.
@@ -39,7 +108,7 @@ public class Grafo<Item> {
         this.arestas = arestas;
     }
 
-    public int[] encontraDistancias(NodeGrafo<Item>[] arestas, int indice) { // fazer busca em largura
+    public int[] encontraDistancias(int indice) { // fazer busca em largura
         int[] distancias = new int[V];
         Queue<Integer> fila = new LinkedList<>();
 
@@ -50,17 +119,11 @@ public class Grafo<Item> {
 
         while (!fila.isEmpty()) {
             int atual = fila.remove();
-            NodeAdjacente aux = arestas[atual].proximo(); // a primeira iteração é feita separadamente, porque arestas[atual]
-                                                            // não é da classe NodeAdjacente, mas da classe NodeGrafo
+            NodeGrafo aux = arestas[atual];
             if (aux != null) {
-                int v = aux.indice();
-                if (distancias[v] == -1) {
-                    distancias[v] = distancias[atual] + 1;
-                    fila.add(v);
-                }
-                for (int j = 0; j < arestas[atual].nAdjacentes() - 1; j++) { // percorrendo os adjacentes de 'atual'
+                for (int j = 0; j < arestas[atual].nAdjacentes(); j++) { // percorrendo os adjacentes de 'atual'
                     aux = aux.proximo();
-                    v = aux.indice();
+                    int v = aux.valor();
                     if (distancias[v] == -1) {
                         distancias[v] = distancias[atual] + 1;
                         fila.add(v);
@@ -74,7 +137,7 @@ public class Grafo<Item> {
     public int[] buscaConexas() { 
         int c = 0;
         boolean[] marcados = new boolean[V];
-        int[] componentes = new int[V];
+        int[] componentes = new int[V + 1];
         for (int i = 0; i < V; i++) {
             marcados[i] = false;
             componentes[i] = -1;
@@ -88,24 +151,28 @@ public class Grafo<Item> {
                 c++;
             }
         }
+        componentes[V] = c; // guarda o número de componentes na última posição do array
         return componentes;
     }
 
     private void dfsR(int v, boolean[] marked) {
         marked[v] = true;
-        NodeAdjacente aux = arestas[v].proximo(); // a primeira iteração é feita separadamente, porque arestas[atual]
-                                                    // não é da classe NodeAdjacente, mas da classe NodeGrafo
+        NodeGrafo aux = arestas[v];
         if (aux != null) {
-            int k = aux.indice();
-            if (!marked[k])
-                dfsR(k, marked);
-
-            for (int i = 0; i < arestas[v].nAdjacentes() - 1; i++) {
+            for (int i = 0; i < arestas[v].nAdjacentes(); i++) {
                 aux = aux.proximo();
-                int w = aux.indice();
+                int w = aux.valor();
                 if (!marked[w])
                     dfsR(w, marked);
             }
         }
+    }
+
+    public static void main (String[] args) throws IOException {
+        int V = 300;
+        Grafo G = new Grafo(V, 0.01);
+        int[] conexas = G.buscaConexas();
+        System.out.println(conexas[V]);
+        G.encontraDistancias(2);
     }
 }
